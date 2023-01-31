4a818d617402fb34adbc9a0f8df3df64b3d6cb73
monkdevloper <abhipippalla@gmail.com>
2023-01-31 17:44:00 +0530

    index port env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
-const port = process.env.PORT;
 
 // const multer = require("multer");
 // const upload = multer({
